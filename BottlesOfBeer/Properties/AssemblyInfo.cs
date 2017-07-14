65ac080e9998b8a2f904e03664a3e2e70166cc4f
Steve Fenton <github@stevefenton.co.uk>
2017-07-14 16:00:34 +0100

    Shameless Green with Tests.

---
+++ BottlesOfBeer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BottlesOfBeer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BottlesOfBeer")]
+[assembly: AssemblyCopyright("Copyright © Steve Fenton 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cf8658ed-bf93-44ad-8d8d-454b54f07bb5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
